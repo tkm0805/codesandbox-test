200345d68638b19c0338f3473d4ee4261aa8e8f7
tkm0805 <61465998+tkm0805@users.noreply.github.com>
2022-04-15 19:10:03 +0900

    配列の展開まで

--- src/index.js
+++ src/index.js
@@ -1,10 +1,110 @@
-import "./styles.css";
+// var vall = "var変数";
+// console.log(vall);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// //var変数は上書き可能
+// vall = "var変数を上書き";
+// console.log(vall);
+
+// // var変数は再宣言可能
+// var vall = "var変数を再宣言";
+// console.log(vall);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name:"じゃけぇ",
+//   age:28,
+// };
+// val4.name = "jak";
+// val4.address = "hokkaidou";
+// console.log(val4);
+
+// constで定義した配列はプロパティの変更が可能
+// const val5 = ["dog","cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+// const name = "tkm";
+// const age = 28;
+//「私の名前はtkmです。年齢は28歳です。」
+
+//従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+
+//テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+//アロー関数
+
+//従来の関数
+// function func1(str) {
+//   return str;
+// };
+// const func1 = function(str) {
+//   return str;
+// };
+// console.log(func1("func1です。"))
+
+//アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です。"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
+
+//分割代入
+
+// const myprofile = {
+//   name:"tkm",
+//   age:28,
+// };
+
+// const message1 = `名前は${myprofile.name}です。年齢は${myprofile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myprofile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myprofile = ["tkm",28];
+
+// const message3 = `名前は${myprofile[0]}です。年齢は${myprofile[1]}歳です。`;
+// console.log(message3);
+
+// const [name, age] = myprofile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello();
+
+/**
+ * スプレッド構文 ...
+ */
+//配列の展開
+// const arr1 = [1,2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1,num2) => console.log(num1+num2);
+// sumFunc(arr1[0],arr1[1]);
+// sumFunc(...arr1);

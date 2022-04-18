8a55a1adb70d21f868319d57c08a0a5b9fd57805
tkm0805 <61465998+tkm0805@users.noreply.github.com>
2022-04-18 13:44:49 +0900

    最終版

--- src/index.js
+++ src/index.js
@@ -108,3 +108,45 @@
 // const sumFunc = (num1,num2) => console.log(num1+num2);
 // sumFunc(arr1[0],arr1[1]);
 // sumFunc(...arr1);
+
+/**
+ * 三項演算子
+ */
+//ある条件? 条件がtrueの時:条件がfalseの時
+// const vall = 1 > 0 ? `trueです` : `falseです`;
+// console.log(vall);
+
+// const num = '1300';
+// // console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください'
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100を超えています" : "許容範囲内です";
+// };
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+// ||は左側がfalseなら右側を返す、左側がtrueなら左側を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す、左側がfalseなら左側を返す
+const num2 = null;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
